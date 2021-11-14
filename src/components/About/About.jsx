7a5779b488e00141738bce4de710da665eb3f549
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2021-11-14 22:54:54 +0400

    create search bar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,9 @@ import { Link } from "react-router-dom";
 function About() {
     useEffect(() => {
         fetchCountries()
-        searchCountry()
     }, [])
     const [countries, setCountries] = useState([])
-    const [search_name, setSearch] = useState({name: "a"}) 
+    const [searchName, setSearchName] = useState('') 
 
     const fetchCountries = () => {
         axios.get("https://corona-api.com/countries")
@@ -21,33 +20,24 @@ function About() {
             .catch((error) => {
                 console.log(error)
             })
-    }
-
-    const change = (e) => {
-        const target = e.target
-        setSearch(() => {
-            return {
-                ...search_name,
-                [target.name]: target.value
-            }
-            
-        })
-    }
-    
-    const searchCountry = () => {
-        console.log(search_name)
-    }
+    }  
 
     return (
         <div>
             <h1>About Coronavirus</h1>
             <h2>Our Countries</h2>
             <div className = "country_search">
-                <input className = "country_search-input" type="search" placeholder = "Search..." onChange = {change} value = {search_name.name} name = 'name'/>
+                <input className = "country_search-input" type="search" placeholder = "Search..." onChange = {(event) => setSearchName(event.target.value)} />
             </div>
             <div>
             {
-                countries.map((item) => (
+                countries.filter((value) => {
+                    if(searchName == ""){
+                        return value.name
+                    } else if(value.name.toLowerCase().includes(searchName.toLowerCase())) {
+                        return value.name
+                    }
+                }).map((item) => (
                     <div className="country_list" key={item.code}>
                         <Link className = "contry_list-link" to = {`/country/${item.name}`} >{item.name}</Link>
                     </div>
